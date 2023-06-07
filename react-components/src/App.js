07e5f0458fd8046effb8e4e9ddb90b06df8c6987
markdaniel1982 <>
2023-06-07 09:47:50 +0000

    create react app and setup FGWP

--- react-components/src/App.js
+++ react-components/src/App.js
@@ -7,8 +7,8 @@ function App() {
   return (
     <div className="App">
       <FunctionalGreeting name="John" />
-      {/* <StatefulGreeting name="Anna" /> */}
-      {/* <HooksCounter name="Mike" /> */}
+      <StatefulGreeting name="Anna" />
+      <HooksCounter name="Mike" />
     </div>
   );
 }
