06dc81279a97f2e6f6e0c4e85f60d7f81672701e
markdaniel1982 <>
2023-06-08 11:18:07 +0000

    rendering lists complete

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -11,6 +11,7 @@ import './App.css';
 // import ConditionalRenderingFunctional from "./components/ConditionalRenderingFunctional"
 // import NestingComponents from './components/NestingComponents'
 // import MethodsAsPropsParent from "./components/MethodsAsPropsParent";
+import RenderingLists from './components/RenderingLists';
 
 function App() {
   return (
@@ -26,6 +27,7 @@ function App() {
       {/* <ConditionalRenderingFunctional connected={true} /> */}
       {/* <NestingComponents /> */}
       {/* <MethodsAsPropsParent /> */}
+      <RenderingLists />
 
     </div>
   );
