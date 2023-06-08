24626d8c0082383867d6404390cb6a3f05e90c72
markdaniel1982 <>
2023-06-08 06:39:51 +0000

    callbacks and prevState

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,23 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 // import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import EventsClass from "./components/EventsClass";
+import EventsFunctional from "./components/EventsFunctional";
 
 function App() {
   return (
     <div className="App">
       {/* <FunctionalGreeting /> */}
-      <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mark" age="40" />
+      {/* <FunctionalGreetingWithProps greeting="Nice to meet you!" name="Mark" age="40" /> */}
+      {/* <StatefulGreeting greeting="I'm a stateful class component!" name="Dave" /> */}
+      {/* <StatefulGreetingWithCallback greeting="I'm a stateful class component!" name="Dave" /> */}
+      {/* <StatefulGreetingWithPrevState /> */}
+      <EventsFunctional />
+      <EventsClass />
     </div>
   );
 }
